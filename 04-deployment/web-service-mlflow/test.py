14cd056d64a679abf3158ddfe26871d5c28a88cd
premsurawut <92110316+surawut-jirasaktavee@users.noreply.github.com>
2022-07-26 23:45:58 +0700

    update

---
+++ 04-deployment/web-service-mlflow/test.py
@@ -0,0 +1,13 @@
+import requests
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40
+}
+
+url = "http://localhost:9696/predict"
+response = requests.post(url, json=ride)
+print(response.json())
+
+print(response)
