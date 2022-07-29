e5e6764cad19de85ecfd95581b85fdf91808fe8e
premsurawut <92110316+surawut-jirasaktavee@users.noreply.github.com>
2022-07-29 19:28:05 +0700

    upload images module 4.4 stream

--- 04-deployment/streaming/test.py
+++ 04-deployment/streaming/test.py
@@ -6,17 +6,17 @@ event = {
             "kinesis": {
                 "kinesisSchemaVersion": "1.0",
                 "partitionKey": "1",
-                "sequenceNumber": "49630081666084879290581185630324770398608704880802529282",
-                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDI1NgogICAgfQ==",
-                "approximateArrivalTimestamp": 1654161514.132
+                "sequenceNumber": "49631818369979129679861768006405126900808089115104903170",
+                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LAogICAgICAgICJyaWRlX2lkIjogMjU2CiAgICB9",
+                "approximateArrivalTimestamp": 1659013681.735
             },
             "eventSource": "aws:kinesis",
             "eventVersion": "1.0",
-            "eventID": "shardId-000000000000:49630081666084879290581185630324770398608704880802529282",
+            "eventID": "shardId-000000000000:49631818369979129679861768006405126900808089115104903170",
             "eventName": "aws:kinesis:record",
-            "invokeIdentityArn": "arn:aws:iam::387546586013:role/lambda-kinesis-role",
-            "awsRegion": "eu-west-1",
-            "eventSourceARN": "arn:aws:kinesis:eu-west-1:387546586013:stream/ride_events"
+            "invokeIdentityArn": "arn:aws:iam::551011018709:role/lambda-kinesis-role",
+            "awsRegion": "us-west-1",
+            "eventSourceARN": "arn:aws:kinesis:us-west-1:551011018709:stream/ride_events"
         }
     ]
 }
